d7650b4abcb28e5cbf8cc7352876f7e2e128313e
iamjahongir2010 <i.jaha2010@gmail.com>
2025-02-14 22:14:17 +0500

    finish contacts

--- src/components/main/contact/Contact.jsx
+++ src/components/main/contact/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import style from './Contact.module.css'
+import React from "react";
+import style from "./Contact.module.css";
+import Button from "react-bootstrap/Button";
+import { Col, Row, Container } from "react-bootstrap";
+
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className={style.Contact}>
+        <h1>Contacts</h1>
+        <h3>Свяжитесь со мной по почте или через соцсети:</h3>
+        <Row className="justify-content-center mt-3">
+          <Col xs="auto">
+            <Button variant="danger" href="mailto:i.jaha2010@mail.ru">Email</Button>
+          </Col>
+          <Col xs="auto">
+            <Button variant="dark" href="https://github.com/iamjahongir2010">GitHub</Button>
+          </Col>
+          <Col xs="auto">
+            <Button variant="primary" href="https://t.me/jaha2010">Telegram</Button>
+          </Col>
+        </Row>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
