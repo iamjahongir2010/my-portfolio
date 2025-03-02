0bffad7d4fba54fced668e04b4f99e9e3849dc6c
iamjahongir2010 <i.jaha2010@gmail.com>
2025-03-02 23:02:36 +0500

    c

--- src/components/main/contact/Contact.jsx
+++ src/components/main/contact/Contact.jsx
@@ -6,19 +6,19 @@ import { Col, Row } from "react-bootstrap";
 function Contact() {
   return (
     <div className={style.Contact}>
-        <h1>Contacts</h1>
-        <h3>Contact me by mail or social media:</h3>
-        <Row className="justify-content-center mt-3">
-          <Col xs="auto">
-            <Button variant="danger" href="mailto:i.jaha2010@mail.ru">Email</Button>
-          </Col>
-          <Col xs="auto">
-            <Button variant="dark" href="https://github.com/iamjahongir2010">GitHub</Button>
-          </Col>
-          <Col xs="auto">
-            <Button variant="primary" href="https://t.me/jaha2010">Telegram</Button>
-          </Col>
-        </Row>
+      <h1>Contacts</h1>
+      <h3>Contact me by mail or social media:</h3>
+      <Row className="justify-content-center mt-3">
+        <Col xs="auto">
+          <Button variant="warning" href="mailto:i.jaha2010@mail.ru">Email</Button>
+        </Col>
+        <Col xs="auto">
+          <Button variant="dark " href="https://github.com/iamjahongir2010">GitHub</Button>
+        </Col>
+        <Col xs="auto">
+          <Button variant="info" href="https://t.me/jaha2010">Telegram</Button>
+        </Col>
+      </Row>
     </div>
   );
 }
